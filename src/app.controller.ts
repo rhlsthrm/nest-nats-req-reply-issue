b4efb019470d7f8b54d29f5f3240dff13dfca3d4
Rahul <rksethuram9@gmail.com>
2019-06-17 11:03:14 +0200

    Simplify app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,18 @@
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import {
-  Client,
-  ClientProxy,
-  Transport,
   MessagePattern,
 } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Client({
-    options: {
-      url: 'nats://localhost:4222',
-    },
-    transport: Transport.NATS,
-  })
-  client: ClientProxy;
+  constructor(
+    private readonly appService: AppService,
+  ) {}
 
   @MessagePattern({ cmd: 'hello' })
   getHello(data: any) {
     console.log('data: ', data);
-    return this.client.send<number>({ cmd: 'hello' }, [1, 2, 3]);
+    return this.appService.getHello();
   }
 }
